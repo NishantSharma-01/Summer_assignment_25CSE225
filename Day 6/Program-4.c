f495972a232b3d22dbee2d81953e9640129b69d1
Nishant Sharma <nishant6398108991@gmail.com>
2026-06-10 13:58:51 +0530

    Create Day 6/Program-4.c

---
+++ Day 6/Program-4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int x, n;
+long long result = 1;
+printf("Enter base (x) and exponent (n): ");
+scanf("%d %d", &x, &n);
+for (int i = 1; i <= n; i++) {
+result = result * x;
+}
+printf("%d^%d = %lld\n", x, n, result);
+return 0;
+}
